8ee88431b96cf3559df17a9b5e6f7fd3962b3b7c
brentongalien981 <brentongalien981@gmail.com>
2021-02-24 15:43:50 -0500

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,18 +16,18 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name')->nullable();
-            $table->string('oauth_provided_user_id', 64);
-            // $table->string('email')->unique();
-            // $table->timestamp('email_verified_at')->nullable();
-            // $table->string('password');
-            // $table->rememberToken();
+            // $table->string('oauth_provided_user_id', 64);
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
         });
 
-        Schema::table('users', function (Blueprint $table) {
+        // Schema::table('users', function (Blueprint $table) {
 
-            $table->foreign('oauth_provided_user_id')->references('oauth_provided_user_id')->on('my_auth_users');
-        });
+        //     $table->foreign('oauth_provided_user_id')->references('oauth_provided_user_id')->on('my_auth_users');
+        // });
     }
 
     /**
